5e9322a8b7174ead7aac86286919903db31e81ab
Thony Nava <41093760+ThonyNava@users.noreply.github.com>
2020-04-09 12:34:42 +0200

    G-string-methods + prettier

--- week-1/G-strings-methods/exercise.js
+++ week-1/G-strings-methods/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
-
+var name = "Thony";
+var nameLowerCase = name.length;
+var message = `My name is ${name}, and is ${nameLowerCase} characters long.`;
 console.log(message);
--- week-1/G-strings-methods/exercise2.js
+++ week-1/G-strings-methods/exercise2.js
@@ -1,3 +1,5 @@
-var name = " Daniel  ";
-
+var usrInput = " Thony ";
+var name = usrInput.trim();
+var nameLowerCase = name.length;
+var message = `My name is ${name}, and is ${nameLowerCase} characters long.`;
 console.log(message);
