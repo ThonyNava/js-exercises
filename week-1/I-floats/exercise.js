bcf8711e17f867d46f58568315f6b86895bb4019
Thony Nava <41093760+ThonyNava@users.noreply.github.com>
2020-04-09 12:54:33 +0200

    I-floats

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,10 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var total = numberOfStudents + numberOfMentors;
+var percentage = 100 / total;
+console.log(
+  `Percentage of students: ${Math.round(numberOfStudents * percentage)}%`
+);
+console.log(
+  `Percentage of mentors: ${Math.round(numberOfMentors * percentage)}%`
+);
